1cc1a6a03a5ad84c1973b2360c854125c6c1f73b
Sagar <04017711721_iot@vvips.edu>
2023-12-01 21:16:53 +0530

    Frame commit

---
+++ Game-Dev/src/Gaming/GameFrame.java
@@ -0,0 +1,13 @@
+package Gaming;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+
+	public static void main(String[] args) {
+		GameFrame Frame = new GameFrame();
+		Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Frame.setSize(1200 , 700 );
+		Frame.setVisible(true);
+	}
+}
